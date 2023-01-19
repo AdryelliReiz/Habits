ae9614f0cc8c794e9e219e4632cb80f30cf74acc
AdryelliReiz <adryellireis0santos@gmail.com>
2023-01-19 17:33:46 -0300

    finish: 2° aula (web)

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+interface HabitDayProps {
+    //completed: number
+}
+
+export function HabitDay(props: HabitDayProps) {
+    return (
+        <div className="bg-zinc-900 w-10 h-10 rounded-lg border-zinc-800">
+            
+        </div>
+    )
+}
